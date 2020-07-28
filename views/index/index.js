d161d3e4094c2a98b08a2032e84f6a581ebf24ea
lxc <coderlxc@gmail.com>
2020-07-28 23:47:30 +0800

    发布时处理图片链接，上传到 B 站

--- views/index/index.js
+++ views/index/index.js
@@ -21,52 +21,53 @@ import db from '../../utils/Db.js'
     }, 500);
 
 
-    // 发布
+    // 点击发布：从 URL 获取图片上传到 B 站获取站内链接并替换，提交数据。
     document.getElementById('btn-publish').addEventListener('click', () => {
-
         db.get(null, data => {
-            axios({
-                method: 'post',
-                url: 'https://api.bilibili.com/x/article/creative/draft/addupdate',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                transformRequest: [
-                    obj => {
-                        let str = ''
-                        for (let [key, value] of Object.entries(obj)) {
-                            str += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&'
-                        }
+            let markdown = document.getElementById('content').value;
+            BilibiliMarkdown(data.csrf, markdown).then(content => {
+                axios({
+                    method: 'post',
+                    url: 'https://api.bilibili.com/x/article/creative/draft/addupdate',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    transformRequest: [
+                        obj => {
+                            let str = ''
+                            for (let [key, value] of Object.entries(obj)) {
+                                str += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&'
+                            }
 
-                        return str.slice(0, -1);
+                            return str.slice(0, -1);
+                        }
+                    ],
+                    data: {
+                        aid: data.aid,
+                        csrf: data.csrf,
+                        title: document.getElementById('title').value,
+                        content: content,
                     }
-                ],
-                data: {
-                    aid: data.aid,
-                    csrf: data.csrf,
-                    title: document.getElementById('title').value,
-                    content: BilibiliMarkdown(data.csrf, document.getElementById('content').value),
-                }
-            }).then(response => {
-                response.data.code === 0 ? alert('成功') : alert('失败');
+                }).then(response => {
+                    response.data.code === 0 ? alert('成功') : alert('失败');
+                });
             });
         });
     });
 
-    function BilibiliMarkdown(csrf, markdown) {
-        const html = new showdown.Converter().makeHtml(markdown);
+    async function BilibiliMarkdown(csrf, markdown) {
+        let html = new showdown.Converter().makeHtml(markdown);
+        let result = [...html.matchAll(/<img src="(.*)" alt="(.*)" \/>/g)];
+        let urls = [];
 
-        const content = html.replace(/<img src="(.*)" alt="(.*)" \/>/g, function (match, src, alt) {
+        result.forEach(value => {
             let image = new Image();
-            image.src = src + '?v=' + Math.random(); // 处理缓存
+            image.src = value[1] + '?v=' + Math.random(); // 处理缓存
             image.crossOrigin = "*";  // 支持跨域图片
             image.onload = () => {
-                let file = dataURLtoBlob(getBase64Image(image));
-                console.log(file);
-
                 let form = new FormData();
                 form.append('csrf', csrf);
-                form.append('binary', file);
+                form.append('binary', dataURLtoBlob(getBase64Image(image)));
 
                 axios({
                     method: 'post',
@@ -76,39 +77,48 @@ import db from '../../utils/Db.js'
                     },
                     data: form,
                 }).then(res => {
-                    console.log('----上传图片结果----');
-                    // {"code":0,"message":"0","ttl":1,"data":{"size":4815447,"url":"http://i0.hdslb.com/bfs/article/cd048a462fccf0349abfc9020d4241247042f312.png"}}
-                    console.log(res);
+                    urls.push([value[1], value[2], res.data.data.url]);
                 });
             }
         });
+
+        while (urls.length < result.length) {
+            await sleep(1000);
+        }
+
+        urls.forEach(value => {
+            html = html.replace(`<img src="${value[0]}" alt="${value[1]}" \/>`, `<img src="${value[2]}" alt="${value[1]}" \/>`);
+        });
+
+        return html;
     }
 
-    /**
-     * 图像转Base64
-     */
+
+    function sleep(time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
     function getBase64Image(img) {
-        var canvas = document.createElement("canvas");
+        let canvas = document.createElement("canvas");
         canvas.width = img.width;
         canvas.height = img.height;
-        var ctx = canvas.getContext("2d");
+        let ctx = canvas.getContext("2d");
         ctx.drawImage(img, 0, 0, img.width, img.height);
-        var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
-        var dataURL = canvas.toDataURL("image/" + ext);
-        return dataURL;
+        let ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+        return canvas.toDataURL("image/" + ext);
     }
 
-    function dataURLtoBlob(dataurl) {
-        var arr = dataurl.split(','),
+    function dataURLtoBlob(dataUrl) {
+        let arr = dataUrl.split(','),
             mime = arr[0].match(/:(.*?);/)[1],
-            bstr = atob(arr[1]),
-            n = bstr.length,
+            str = atob(arr[1]),
+            n = str.length,
             u8arr = new Uint8Array(n);
+
         while (n--) {
-            u8arr[n] = bstr.charCodeAt(n);
+            u8arr[n] = str.charCodeAt(n);
         }
-        return new Blob([u8arr], {
-            type: mime
-        });
+
+        return new Blob([u8arr], {type: mime});
     }
 })();
